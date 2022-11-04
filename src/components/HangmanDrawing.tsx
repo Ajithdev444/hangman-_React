4af96f5886e97a6e49f85a7aedd0c35ca22ca2b3
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-04 16:22:37 +0530

    Hangman drawing

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,48 @@
+const HEAD = (
+  <div style={{
+    width:'50px',
+    height:'50px',
+    borderRadius:'100%',
+    border:'10px solid black',
+    position:'absolute',
+    top:'50px',
+    right:'-30px',
+  }}/>
+)
+
+const HangmanDrawing = () => {
+  return (
+    <div style={{ position:'relative'}}>
+      <div 
+      style={{
+        height:'50px',
+        width:'10px',
+        background:'black',
+        top: 0,
+        right: 0,
+        position:'absolute',
+      }}
+      />
+      <div 
+      style={{
+        height:'10px',
+        width:'200px',
+        background:'black',
+        marginLeft:'120px',
+      }}
+      />
+      <div 
+      style={{
+        height:'400px',
+        width:'10px',
+        background:'black',
+        marginLeft:'120px',
+      }}
+      />
+      <div style={{ height:'10px', width:'250px', background:'black'}}/>
+
+    </div>
+  )
+}
+
+export default HangmanDrawing
