b31f7a6c4595b8e7c18ff4ca582c2845bea26148
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-04 17:02:50 +0530

    hangmanwords

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
 
 const HangmanWord = () => {
+  const word = 'test'
+  const guessedLetters = ['t','a', 's']
   return (
-    <div>HangmanWord</div>
+    <div style={{
+       display:'flex',
+       gap:'.25rem',
+       fontSize:'6rem',
+       fontWeight:'bold', 
+       textTransform:'uppercase',
+       fontFamily:'monospace'}} >
+
+        {word.split('').map(( letter, index ) => (
+          <span style={{ borderBottom:'.1em solid black'}} key={index}>
+            <span style={{
+              visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden',
+            }}>{letter}</span>
+            </span>
+        ))}
+    </div>
   )
 }
 
