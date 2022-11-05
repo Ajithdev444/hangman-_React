b7b444ed5baa194eb5acaeae7c0792294e75e8c3
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-05 09:00:30 +0530

    keyword

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import HangmanDrawing from "./components/HangmanDrawing"
 import HangmanWord from "./components/HangmanWord"
 import Keyboard from "./components/Keyboard"
@@ -10,6 +10,29 @@ const [wordToGuess, setWordToGuess] = useState(() =>{
   return words[Math.floor(Math.random() * words.length)]
 })
 const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+
+function addGuessedLetter(letter: string) {
+if(guessedLetters.includes(letter)) return
+
+setGuessedLetters(currentLetters => [...currentLetters, letter])
+}
+
+useEffect(() =>{
+const handler = (e: KeyboardEvent) => {
+  const key = e.key
+  if(!key.match(/^[a-z]$/)) return 
+  e.preventDefault()
+  addGuessedLetter(key)
+
+}
+document.addEventListener('keypress',handler)
+
+return () => {
+document.removeEventListener('keypress', handler)
+}
+},[])
 
   return (
     <div 
@@ -23,9 +46,11 @@ const [guessedLetters, setGuessedLetters] = useState<string[]>([])
       }}
     >
       <div style={{ fontSize:'2rem', textAlign:'center',}}>Lose Win</div>
-      <HangmanDrawing/>
-      <HangmanWord/>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
+      <div style={{ alignSelf:'stretch'}}>
       <Keyboard/>
+      </div>
      
   </div>
   )
--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,9 @@
+type HangmanWordProps ={
+  guessedLetters: string[]
+  wordToGuess: string
+}
 
-const HangmanWord = () => {
-  const word = 'test'
-  const guessedLetters = ['t','a', 's']
+const HangmanWord = ({ guessedLetters, wordToGuess}: HangmanWordProps) => {
   return (
     <div style={{
        display:'flex',
@@ -11,7 +13,7 @@ const HangmanWord = () => {
        textTransform:'uppercase',
        fontFamily:'monospace'}} >
 
-        {word.split('').map(( letter, index ) => (
+        {wordToGuess.split('').map(( letter, index ) => (
           <span style={{ borderBottom:'.1em solid black'}} key={index}>
             <span style={{
               visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden',
